5f997ad82679d3639254618e154c0f1974f2ad01
jmtr1 <87749945+jmtr1@users.noreply.github.com>
2025-06-30 12:25:43 +0000

    update shell

--- initialise.sh
+++ initialise.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # This init script presents various ways of installing Python packages
-GIT_REPO=ai-patents-and-innovation
-python -m pip install -r ${GIT_REPO}/requirements.txt
+python -m pip install torch dask transformers ipywidgets boto3
