db33a2ec1f7de96a7fc3df0730bc30c17d5cafc5
jmtr1 <87749945+jmtr1@users.noreply.github.com>
2025-09-08 11:33:43 +0200

    Update initialise.sh

--- initialise.sh
+++ initialise.sh
@@ -4,4 +4,5 @@ set -e
 
 # Install GDAL Python package with numpy-based raster support
 # See : https://pypi.org/project/GDAL/
-uv pip install --system torch dask transformers ipywidgets boto3 openai dotenv optuna lightgbm wandb openpyxl
+uv pip install --system torch dask transformers ipywidgets boto3 openai dotenv optuna lightgbm wandb openpyxl botocore-1.40.18
+
