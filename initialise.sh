b9dd3a0ed31aaf837f999735848b4c7c74e14eca
jmtr1 <87749945+jmtr1@users.noreply.github.com>
2025-10-02 14:51:29 +0200

    Update initialise.sh
    Update with theme and icon set

--- initialise.sh
+++ initialise.sh
@@ -2,8 +2,20 @@
 
 set -e
 
-# Install GDAL Python package with numpy-based raster support
-# See : https://pypi.org/project/GDAL/
+# Install Python packages
 uv pip install --system torch dask transformers ipywidgets boto3 openai dotenv optuna lightgbm wandb openpyxl nbconvert botocore==1.40.18
 
+# Install VS Code extensions
+code --install-extension PKief.material-icon-theme
+code --install-extension miguelcorraljr.jupyterlab-light-theme
 
+# Ensure VS Code settings directory exists
+mkdir -p ~/.config/Code/User
+
+# Write VS Code settings
+cat > ~/.config/Code/User/settings.json <<EOF
+{
+  "workbench.iconTheme": "material-icon-theme",
+  "workbench.colorTheme": "JupyterLab Light Theme"
+}
+EOF
