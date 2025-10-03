7580fd8a349b4e0c96dfc89a7849cd440db395c4
jmtr1 <87749945+jmtr1@users.noreply.github.com>
2025-10-03 15:04:31 +0200

    Update init_new.sh

--- init_new.sh
+++ init_new.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
+exec > >(tee -a ~/init.log) 2>&1
 set -euxo pipefail
 
 echo "[Init] Starting VSIX download and install"
 
-# Download
 wget -O /tmp/material-icon-theme.vsix \
   https://github.com/jmtr1/temp/raw/refs/heads/main/pkief.material-icon-theme-5.27.0.vsix
 
 ls -lh /tmp/material-icon-theme.vsix
 
-# Install into code-server
 code-server --install-extension /tmp/material-icon-theme.vsix --force
 
-# Confirm installation
 echo "[Init] Installed extensions are now:"
 code-server --list-extensions || true
 
