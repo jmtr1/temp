40236bcf754dc286945c62f88faeb1de593251cb
jmtr1 <87749945+jmtr1@users.noreply.github.com>
2025-10-03 15:14:51 +0200

    Update init_new.sh

--- init_new.sh
+++ init_new.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-echo "Hello from init.sh" > ~/init.log
-date >> ~/init.log
+exec > >(tee -a /home/onyxia/init.log) 2>&1
+set -euxo pipefail
+
+echo "[Init] Running init.sh"
+
+# Example step
+date
+
+# Download and install
+wget -O /tmp/material-icon-theme.vsix \
+  https://github.com/jmtr1/temp/raw/refs/heads/main/pkief.material-icon-theme-5.27.0.vsix
+code-server --install-extension /tmp/material-icon-theme.vsix --force
