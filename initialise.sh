e4d6637ec8977d62433fc128e9af42d971bcf7a5
jmtr1 <87749945+jmtr1@users.noreply.github.com>
2025-10-03 15:55:38 +0200

    Update initialise.sh

--- initialise.sh
+++ initialise.sh
@@ -12,16 +12,16 @@ chmod +x "$INKSCAPE_APPIMAGE"
 ./squashfs-root/AppRun --version
 sudo ln -sf "$(pwd)/squashfs-root/AppRun" /usr/local/bin/inkscape
 
-
-# Follow redirects and write straight to a .vsix file
-curl -L \
-  "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/PKief/vsextensions/material-icon-theme/5.27.0/vspackage" \
-  -o material-icon-theme.vsix
-
-code-server --install-extension material-icon-theme.vsix --force
-rm material-icon-theme.vsix
-
 # Cleanup Inkscape files
 rm -rf squashfs-root
 rm -f "$INKSCAPE_APPIMAGE"
+
+materialIconVersion="5.27.0"
+wget --retry-on-http-error=429 "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/PKief/vsextensions/material-icon-theme/${materialIconVersion}/vspackage" -O material-icon-theme.vsix.gz
+gzip -d material-icon-theme.vsix.gz
+code-server --install-extension material-icon-theme.vsix --force
+rm material-icon-theme.vsix
+
+
+
 
