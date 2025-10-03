2ce5e270b6d4d332c2f6cc2ddcc76bd1f452d64e
jmtr1 <87749945+jmtr1@users.noreply.github.com>
2025-10-03 15:45:04 +0200

    Update init_new.sh

--- init_new.sh
+++ init_new.sh
@@ -1,26 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-# This init script install various useful VScode extensions
-# Expected parameters : None
+# Install required system packages
+sudo apt-get update
+sudo apt-get install -y ghostscript pstoedit wget jq curl
 
-# CONFORT EXTENSIONS -----------------
-
-# Colorizes the indentation in front of text
-code-server --install-extension oderwat.indent-rainbow
-# Extensive markdown integration
-code-server --install-extension yzhang.markdown-all-in-one
-# Integrates Excalidraw (software for sketching diagrams)
-code-server --install-extension pomdtr.excalidraw-editor
-
-# COPILOT ----------------------------
-
-# Install Copilot (Microsoft's AI-assisted code writing tool)
-copilotVersion="1.171.0"
-copilotChat="0.13.0"
-
-wget --retry-on-http-error=429 "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot/${copilotVersion}/vspackage" -O copilot.vsix.gz
-wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot-chat/${copilotChat}/vspackage -O copilot-chat.vsix.gz
-
-code-server --install-extension copilot.vsix
-code-server --install-extension copilot-chat.vsix
-rm copilot.vsix copilot-chat.vsix
+# Install Inkscape AppImage
+INKSCAPE_APPIMAGE="inkscape.appimage"
+wget https://sourceforge.net/projects/inkscape/files/Inkscape-ebf0e94-x86_64.AppImage/download -O "$INKSCAPE_APPIMAGE"
+chmod +x "$INKSCAPE_APPIMAGE"
+./"$INKSCAPE_APPIMAGE" --appimage-extract
+./squashfs-root/AppRun --version
+sudo ln -sf "$(pwd)/squashfs-root/AppRun" /usr/local/bin/inkscape
