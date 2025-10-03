93ba68b5586c3c7d25706dd0d63af8fe6b4d51f4
jmtr1 <87749945+jmtr1@users.noreply.github.com>
2025-10-03 15:26:17 +0200

    Update init_new.sh

--- init_new.sh
+++ init_new.sh
@@ -2,5 +2,5 @@
 
 # Install icon theme
 wget --retry-on-http-error=429 https://github.com/jmtr1/temp/raw/refs/heads/main/pkief.material-icon-theme-5.27.0.vsix
-code-server --install-extension material-icon-theme.vsix
+code-server --install-extension pkief.material-icon-theme-5.27.0.vsix
 rm material-icon-theme.vsix
