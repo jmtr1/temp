d0d0872caa623c073629758636e5bc4f2b620ed7
jmtr1 <87749945+jmtr1@users.noreply.github.com>
2025-10-03 15:33:56 +0200

    Update init_new.sh

--- init_new.sh
+++ init_new.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+set -e
 
-# Install icon theme
-wget --retry-on-http-error=429 https://github.com/jmtr1/temp/raw/refs/heads/main/pkief.material-icon-theme-5.27.0.vsix
-code-server --install-extension pkief.material-icon-theme-5.27.0.vsix
-rm pkief.material-icon-theme-5.27.0.vsix
+# Download gzipped VSIX from Marketplace and save with a known name
+wget -O material-icon-theme.vsix.gz \
+  "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/PKief/vsextensions/material-icon-theme/5.27.0/vspackage"
+
+# Decompress to VSIX
+gzip -d material-icon-theme.vsix.gz
+
+# Install into code-server
+code-server --install-extension material-icon-theme.vsix --force
+
+# Cleanup
+rm material-icon-theme.vsix
