2d9d3700a33f65ce10b88440f7156875454b9881
jmtr1 <87749945+jmtr1@users.noreply.github.com>
2025-10-03 16:10:12 +0200

    Update initialise.sh

--- initialise.sh
+++ initialise.sh
@@ -1,33 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
-# Install required system packages
-sudo apt-get update
-sudo apt-get install -y ghostscript pstoedit wget jq curl
+# This init script install various useful VScode extensions
+# NB : only extensions from the Open VSX Registry (https://open-vsx.org/) can be installed on code-server
+# Expected parameters : None
 
-# Install Inkscape AppImage
-INKSCAPE_APPIMAGE="inkscape.appimage"
-wget https://sourceforge.net/projects/inkscape/files/Inkscape-ebf0e94-x86_64.AppImage/download -O "$INKSCAPE_APPIMAGE"
-chmod +x "$INKSCAPE_APPIMAGE"
-./"$INKSCAPE_APPIMAGE" --appimage-extract
-./squashfs-root/AppRun --version
-sudo ln -sf "$(pwd)/squashfs-root/AppRun" /usr/local/bin/inkscape
+# CONFORT EXTENSIONS -----------------
 
-# Cleanup Inkscape files
-rm -rf squashfs-root
-rm -f "$INKSCAPE_APPIMAGE"
+# Colorizes the indentation in front of text
+code-server --install-extension oderwat.indent-rainbow
+# Extensive markdown integration
+code-server --install-extension yzhang.markdown-all-in-one
+# Integrates Excalidraw (software for sketching diagrams)
+code-server --install-extension pomdtr.excalidraw-editor
 
-# Install and persist Material Icon Theme extension
-materialIconVersion="5.27.0"
-MATERIAL_VSIX="material-icon-theme.vsix"
+# COPILOT ----------------------------
 
-# Download to working dir
-wget "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/PKief/vsextensions/material-icon-theme/${materialIconVersion}/vspackage" -O "$MATERIAL_VSIX"
+# Install Copilot (Microsoft's AI-assisted code writing tool)
+copilotVersion="1.234.0"
+copilotChatVersion="0.20.0" # This version is not compatible with VSCode server 1.92.2
 
-# Also copy it to /usr/local/bin for persistence
-sudo cp "$MATERIAL_VSIX" /usr/local/bin/
+wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot/${copilotVersion}/vspackage -O copilot.vsix.gz
+wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot-chat/${copilotChatVersion}/vspackage -O copilot-chat.vsix.gz
 
-# Install it into code-server
-code-server --install-extension "$MATERIAL_VSIX" --force
+gzip -d copilot.vsix.gz 
+gzip -d copilot-chat.vsix.gz 
 
-# Cleanup local working copy
-rm "$MATERIAL_VSIX"
+code-server --install-extension copilot.vsix
+code-server --install-extension copilot-chat.vsix
+rm copilot.vsix copilot-chat.vsix
