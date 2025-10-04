1f5196037c6620c4739fc30db2d5518720ad698f
jmtr1 <87749945+jmtr1@users.noreply.github.com>
2025-10-04 12:23:29 +0200

    Update initialise.sh

--- initialise.sh
+++ initialise.sh
@@ -6,7 +6,7 @@ wget https://sourceforge.net/projects/inkscape/files/Inkscape-ebf0e94-x86_64.App
 chmod +x "$INKSCAPE_APPIMAGE"
 ./"$INKSCAPE_APPIMAGE" --appimage-extract
 ./squashfs-root/AppRun --version
-sudo ln -sf "$(pwd)/squashfs-root/AppRun" /usr/local/bin/inkscape
+#sudo ln -sf "$(pwd)/squashfs-root/AppRun" /usr/local/bin/inkscape
 
 # Install Material Icon Theme extension
 wget --retry-on-http-error=429 https://github.com/jmtr1/temp/raw/refs/heads/main/pkief.material-icon-theme-5.27.0.vsix -O material-icon-theme.vsix
@@ -50,7 +50,8 @@ jq '. + {
         "**/.vscode": true,
         "**/*.lnk": true,
         "**/requirements.txt": true,
-        "**/.cache": true
+        "**/.cache": true,
+        "**/squashfs-root": true
     }
 }' "$SETTINGS_FILE" > "$SETTINGS_FILE.tmp" && mv "$SETTINGS_FILE.tmp" "$SETTINGS_FILE"
 
