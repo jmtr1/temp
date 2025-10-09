6c6542d42327e6327450a0a85b78847532fdbe9f
jmtr1 <87749945+jmtr1@users.noreply.github.com>
2025-10-09 12:32:42 +0200

    Drop inkscape

--- initialise.sh
+++ initialise.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Install Inkscape AppImage
-INKSCAPE_APPIMAGE="inkscape.appimage"
-wget https://sourceforge.net/projects/inkscape/files/Inkscape-ebf0e94-x86_64.AppImage/download -O "$INKSCAPE_APPIMAGE"
-chmod +x "$INKSCAPE_APPIMAGE"
-./"$INKSCAPE_APPIMAGE" --appimage-extract
-./squashfs-root/AppRun --version
+#INKSCAPE_APPIMAGE="inkscape.appimage"
+#wget https://sourceforge.net/projects/inkscape/files/Inkscape-ebf0e94-x86_64.AppImage/download -O "$INKSCAPE_APPIMAGE"
+#chmod +x "$INKSCAPE_APPIMAGE"
+#./"$INKSCAPE_APPIMAGE" --appimage-extract
+#./squashfs-root/AppRun --version
 #sudo ln -sf "$(pwd)/squashfs-root/AppRun" /usr/local/bin/inkscape
 
 # Install system dependencies and fonts
